54b64f0be8746c47811c9e56d921720ff2e9124e
h8254201 <h82542011@gmail.com>
2019-01-19 02:24:04 +0800

    feat(memoryNote): Added memory view to ViewController

---
+++ DesignFlower/MemoryNoteTableViewCell.swift
@@ -0,0 +1,84 @@
+//
+//  MemoryNoteTableViewCell.swift
+//  2017-dishrank-ios
+//
+//  Created by larvata_ios on 2018/7/3.
+//
+
+import UIKit
+
+class MemoryNoteTableViewCell: UITableViewCell {
+  @IBOutlet weak var dishImg: UIImageView!
+  @IBOutlet weak var titleLbl: UILabel!
+  @IBOutlet weak var restNameLbl: UILabel!
+  @IBOutlet weak var cityLbl: UILabel!
+  @IBOutlet weak var dishReviewCountLbl: UILabel!
+  @IBOutlet weak var picsCountLbl: UILabel!
+  @IBOutlet weak var isLikeBt: UIButton!
+  @IBOutlet weak var addressLabel: UILabel!
+  @IBOutlet weak var phoneLabel: UILabel!
+  @IBOutlet weak var toRestStackView: UIStackView!
+  
+  override func awakeFromNib() {
+    super.awakeFromNib()
+    
+    // Initialization code
+  }
+  
+    func configCell(memoryNoteData: RLMRestReviewV4) {
+        titleLbl.text = memoryNoteData.title
+        
+        if memoryNoteData.restaurant == nil {
+            cityLbl.isHidden = true
+        }else{
+            cityLbl.isHidden = false
+        }
+        
+        let restCityAttribute = NSMutableAttributedString(string: "city")
+        restCityAttribute
+            .addAttribute(NSAttributedStringKey.foregroundColor,
+                          value: UIColor.init(red: 186/255,
+                                              green: 143/255,
+                                              blue: 92/255,
+                                              alpha: 1.0),
+                          range: NSMakeRange(0, 1))
+        cityLbl.attributedText = restCityAttribute
+        
+        restNameLbl.text = memoryNoteData.restaurant?.name
+        
+        dishReviewCountLbl.text = memoryNoteData.dishReviews.count.description + "道菜"
+        picsCountLbl.text = memoryNoteData.dishReviews.reduce(0, { (result, dishReview) -> Int in
+            return (result + dishReview.images.count)
+        }).description + "張照片"
+        phoneLabel.text = memoryNoteData.restaurant?.phoneNumber
+        
+        let address = memoryNoteData.restaurant?.address ?? "address"
+        addressLabel.text = address
+        
+        
+        isLikeBt.isHidden = true
+        selectionStyle = .none
+        
+        
+        if let localName = memoryNoteData.dishReviews.first?.images.first?.localName {
+            let localImagePath = KVOImageV4.localFolder.appendingPathComponent(localName)
+            let image = UIImage(contentsOfFile: localImagePath.absoluteString)
+            dishImg.image = image
+        } else {
+            dishImg.image = UIImage(named: "restaurant_placeholder")
+        }
+//        if let urlString = memoryNoteData.dishReviews.first?.images.first?.url {
+//            let url = URL(string: urlString)
+//            dishImg.sd_setImage(with: url)
+//        } else {
+//            dishImg.image = nil
+//        }
+    }
+  override func setSelected(_ selected: Bool, animated: Bool) {
+    super.setSelected(selected, animated: animated)
+
+    // Configure the view for the selected state
+  }
+
+}
+
--- DesignFlower/ViewController.swift
+++ DesignFlower/ViewController.swift
@@ -9,55 +9,62 @@
 import UIKit
 
 class ViewController: UIViewController,
-                      V4ReviewFlowController.Delegate {
-  
-  var reviewFlowController: V4ReviewFlowController?
-  @IBOutlet weak var tableView: UITableView!
-  var list : [RLMRestReviewV4] = []
-  override func viewDidLoad() {
-    super.viewDidLoad()
-    tableView.delegate = self
-    tableView.dataSource = self
-  }
-  
-  override func viewDidAppear(_ animated: Bool) {
-    super.viewDidAppear(animated)
-
-  }
-  
-  @IBAction func clickedWriteReview(_ sender: Any) {
-    reviewFlowController = V4ReviewFlowController(scenario: .writeBegin)
-    reviewFlowController?.delegate = self
-    reviewFlowController?.prepare()
-    reviewFlowController?.start()
-  }
-  
-  
-  
-  func getDisplayContext(for sender: V4ReviewFlowController) -> DisplayContext {
-    return .present(vc: self, animated: true, style: .fullScreen)
-  }
-
+V4ReviewFlowController.Delegate {
+    
+    var reviewFlowController: V4ReviewFlowController?
+    @IBOutlet weak var tableView: UITableView!
+    var list : [RLMRestReviewV4] = []
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let nib = UINib(nibName: "MemoryNoteTableViewCell",
+                        bundle: nil)
+        tableView.register(nib,
+                           forCellReuseIdentifier: "MemoryNoteTableViewCell")
+        tableView.rowHeight = 152
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+    }
+    
+    @IBAction func clickedWriteReview(_ sender: Any) {
+        reviewFlowController = V4ReviewFlowController(scenario: .writeBegin)
+        reviewFlowController?.delegate = self
+        reviewFlowController?.prepare()
+        reviewFlowController?.start()
+    }
+    
+    
+    
+    func getDisplayContext(for sender: V4ReviewFlowController) -> DisplayContext {
+        return .present(vc: self, animated: true, style: .fullScreen)
+    }
+    
 }
 
 extension ViewController: UITableViewDelegate, UITableViewDataSource {
-  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-    list = RLMServiceV4.shared.getRestReviewList() ?? []
-    return list.count
-  }
-
-  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-    let cell = tableView.dequeueReusableCell(withIdentifier: "ReviewCell", for: indexPath)
-    if let cell = cell as? ReviewCell {
-      cell.title.text = list[indexPath.row].title
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        list = RLMServiceV4.shared.getRestReviewList() ?? []
+        return list.count
     }
-    return cell
-  }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MemoryNoteTableViewCell", for: indexPath)
+        if let cell = cell as? MemoryNoteTableViewCell {
+            cell.configCell(memoryNoteData: list[indexPath.row])
+        }
+        return cell
+    }
 }
 
 class ReviewCell: UITableViewCell {
-
-  @IBOutlet weak var title: UILabel!
+    
+    @IBOutlet weak var title: UILabel!
 }
 
 
